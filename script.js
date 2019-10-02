6e2eb7cbb21bb0e0b3735ecf01ad5fc4e9047a17
David Carlson <41599190+Writhyn@users.noreply.github.com>
2019-10-02 14:41:56 -0400

    added event listener to document for space key

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 var button = document.querySelector("#getRandom");
-        var text = document.querySelector("#matthew").innerHTML;
-        var displayDiv = document.querySelector("#displayText");
+var text = document.querySelector("#matthew").innerHTML;
+var displayDiv = document.querySelector("#displayText");
 
-        // Finds the ends of sentences and questions (including dialogue), and adds "@" for easy splitting.
-        text = text.replace(/\. /g, ".@").replace(/” /g, '”@').replace(/\.’ /g, ".’@").replace(/\? /g, "?@");
-        var textArray = text.split("@");
-        var limit = textArray.length
+// Finds the ends of sentences and questions (including dialogue), and adds "@" for easy splitting.
+text = text.replace(/\. /g, ".@").replace(/” /g, '”@').replace(/\.’ /g, ".’@").replace(/\? /g, "?@");
+var textArray = text.split("@");
+var limit = textArray.length
 
-        // When button is clicked, generate random number based on how many array elements.
-        // Then displays the sentence in the window.
-        button.addEventListener("click", function() {
-            var randomNumber = Math.floor(Math.random() * limit);
-            var randomSentence = textArray[randomNumber];
-            if (randomSentence.includes("“") && !randomSentence.includes("”")) {
-                randomSentence = randomSentence + " ...”";
-            } else if (!randomSentence.includes("“") && randomSentence.includes("”")) {
-                randomSentence = "“... " + randomSentence;
-            }
-            displayDiv.innerHTML = randomSentence;
-            displayDiv.classList.add("textEnter");
-            setTimeout ( function() {
-                displayDiv.classList.remove("textEnter");
-            }, 1000);
-        })
+function deliverRandom() {
+    var randomNumber = Math.floor(Math.random() * limit);
+    var randomSentence = textArray[randomNumber];
+    if (randomSentence.includes("“") && !randomSentence.includes("”")) {
+        randomSentence = randomSentence + " ...”";
+    } else if (!randomSentence.includes("“") && randomSentence.includes("”")) {
+        randomSentence = "“... " + randomSentence;
+    }
+    displayDiv.innerHTML = randomSentence;
+    displayDiv.classList.add("textEnter");
+    setTimeout ( function() {
+        displayDiv.classList.remove("textEnter");
+    }, 1000);
+}
+
+// When button is clicked, generate random number based on how many array elements.
+// Then displays the sentence in the window.
+button.addEventListener("click", deliverRandom);
+
+document.addEventListener("keypress", function(e) {
+    if (e.which === 32) {
+        deliverRandom();
+    }
+})
