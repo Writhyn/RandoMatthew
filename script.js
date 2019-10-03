9999b3482594c0b98cf656a74cd266af96c9fb36
David Carlson <41599190+Writhyn@users.noreply.github.com>
2019-10-02 20:22:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-var button = document.querySelector("#getRandom");
-var text = document.querySelector("#matthew").innerHTML;
-var displayDiv = document.querySelector("#displayText");
+const randButton = document.querySelector("#getRandom");
+const text = document.querySelector("#matthew").textContent;
+const displayDiv = document.querySelector("#displayText");
+const contextCheck = document.querySelector("#contextCheck");
+let number = 0;
+let sentence = "";
 
 // Finds the ends of sentences and questions (including dialogue), and adds "@" for easy splitting.
-text = text.replace(/\. /g, ".@").replace(/” /g, '”@').replace(/\.’ /g, ".’@").replace(/\? /g, "?@");
-var textArray = text.split("@");
-var limit = textArray.length
+textToSplit = text.replace(/\. /g, ".@").replace(/\! /g, "!@").replace(/” /g, '”@').replace(/\.’ /g, ".’@").replace(/\? /g, "?@");
+const textArray = textToSplit.split("@");
+const limit = textArray.length;
 
-function deliverRandom() {
-    var randomNumber = Math.floor(Math.random() * limit);
-    var randomSentence = textArray[randomNumber];
-    if (randomSentence.includes("“") && !randomSentence.includes("”")) {
-        randomSentence = randomSentence + " ...”";
-    } else if (!randomSentence.includes("“") && randomSentence.includes("”")) {
-        randomSentence = "“... " + randomSentence;
-    }
-    displayDiv.innerHTML = randomSentence;
+//generate random number, determine if start/end quotes are needed, then display sentence.
+
+
+function checkForQuotes() {
+    if (sentence.includes("“") && !sentence.includes("”")) {
+        sentence = sentence + " ...”";
+    } else if (!sentence.includes("“") && sentence.includes("”")) {
+        sentence = "“... " + sentence;
+    } 
+}
+
+function deliverRandomSentence() {
+    number = Math.floor(Math.random() * limit);
+    sentence = textArray[number];
+
+    checkForQuotes();
+
+    displayDiv.textContent = sentence;
+
+    //Animation
     displayDiv.classList.add("textEnter");
     setTimeout ( function() {
         displayDiv.classList.remove("textEnter");
     }, 1000);
 }
 
-// When button is clicked, generate random number based on how many array elements.
-// Then displays the sentence in the window.
-button.addEventListener("click", deliverRandom);
+function checkContext() {
+    number++;
+    sentence = textArray[number];
+    checkForQuotes();
+    displayDiv.textContent = sentence;
+    
+}
+
+
+//Event listeners for button and keypress
+randButton.addEventListener("click", deliverRandomSentence);
+
+contextCheck.addEventListener("click", checkContext);
 
 document.addEventListener("keypress", function(e) {
     if (e.which === 32) {
-        deliverRandom();
+        deliverRandomSentence();
     }
 })
