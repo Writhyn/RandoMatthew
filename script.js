3faf970a8c904e379b5ada95c905a9052cb81ca5
David Carlson <41599190+Writhyn@users.noreply.github.com>
2019-10-02 21:42:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,17 @@ let number = 0;
 let sentence = "";
 
 // Finds the ends of sentences and questions (including dialogue), and adds "@" for easy splitting.
-textToSplit = text.replace(/\. /g, ".@").replace(/\! /g, "!@").replace(/” /g, '”@').replace(/\.’ /g, ".’@").replace(/\? /g, "?@");
+
+const replaceMap = {
+    '. ': '.@',
+    '? ': '?@',
+    '! ': '!@',
+    '” ': '”@',
+    '.’ ': '.’@',
+};
+const textToSplit = text.replace(/\.|\?/g, char => replaceMap[char]);
+
+// textToSplit = text.replace(/\. /g, ".@").replace(/\! /g, "!@").replace(/” /g, '”@').replace(/\.’ /g, ".’@").replace(/\? /g, "?@");
 const textArray = textToSplit.split("@");
 const limit = textArray.length;
 
